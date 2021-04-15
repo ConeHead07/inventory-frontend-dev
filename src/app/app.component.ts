3fd0ae35fab984b88383a7f7bff28a2afe9a215a
MAG\f.barthold <f.barthold@mertens.ag>
2021-04-15 08:30:15 +0200

    No New-Version-Auto-Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,6 +68,7 @@ export class AppComponent {
         });
  */
   setupUpdates() {
+    return;
     this.swUpdate.available.subscribe(event => {
       // Update wurde entdeckt
       console.log('current version is', event.current);
