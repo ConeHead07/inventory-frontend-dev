1651f502b19ac8a7a874391db9725b35bc0877b6
MAG\f.barthold <f.barthold@mertens.ag>
2021-05-07 10:51:35 +0200

    Set API-URL for rc to dev and for *.web.app

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -41,12 +41,15 @@ export class ApiService {
       case 'demo-pc-app.mertens.services':
         defaultUrl = 'https://' + hostName.replace('-app.mertens.services', '-admin.mertens.services') + '/';
         break;
+
+      case 'mertens-inventory-rc.web.app':
+      case 'mertens-inventory-rc.firebaseapp.com':
+      case 'mertens-inventory-dev.web.app':
       case 'mertens-inventory-dev.firebaseapp.com':
       case 'dev-inventory-app.mertens.services':
         defaultUrl = 'https://dev-inventory.mertens.services/';
         break;
 
-      case 'mertens-inventory-rc.firebaseapp.com':
       case 'mertens-inventory.firebaseapp.com':
       case 'mertens-inventory.web.app':
       default:
